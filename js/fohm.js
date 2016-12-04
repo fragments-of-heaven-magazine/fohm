41cfbe53020ef6d2b0d91c6e8bc0bc10ba29817d
Antoine Foggs <antoine.foggs@gmail.com>
2016-12-04 03:11:19 -0600

    Edited index.html and .js file so that the api is loaded properly. Button still does't work, however.

--- js/fohm.js
+++ js/fohm.js
@@ -1,36 +1,18 @@
- TODO: Use a function closure and release global $
+//TODO: Use a function closure and release global $
 $(document).ready(function() {
-
-  $('#go').on('submit', function(event) {
-    event.preventDefault();
-
-    // let name = document.getElementById('nameS').value;
-    // getUserName(name);
-    getQuote();
-  });
-
-  function getQuote(){
+//  $('#go').on('submit', function(event) {
+    //   event.preventDefault();
+   // var query = $('#name-label').val();
     $.get(
-      'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+      'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       function(data) {
+       // data.preventDefault();
         var q = data[0];
-        // console.log(q.content);
+        console.log(q.content);
+        console.log(q.title);
         $('#primary').append(q.content);
 
-          // var list = '<ul id="random-quote">' +
-          // '<li class="name-label" id="your-name">Your Name</li>' +
-          // '<li class="name">' + name + '</li>' +
-          // '<li class="quote-label" id="your-quote">Your Quote</li>' +
-          // '<li class="quote">' + q.content + '</li>' +
-          // '</ul>';
-
-          // $('#primary').append(list);
         });
-  }
+//});
 
-  // function getUserName(input){
-  //   var names='<p>Welcome '+input+'!</p>';
-  //   $('#username').append(names);
-  // }
-};
-
+}); 
