5282bfc394a158d716370d55645bd66d2c848530
Antoine Foggs <antoine.foggs@gmail.com>
2016-12-03 21:54:52 -0600

    Saving data for Ore so that she could perform some tweaks on what we've already changed.

--- js/fohm.js
+++
@@ -1,5 +0,0 @@
-// TODO: Use a function closure and release global $
-$(document).ready(function() {
-
-      $('#inspirational').html('<img id="thePicture" src=' + movie + ' alt = "Poster Not Found" />');
-});
