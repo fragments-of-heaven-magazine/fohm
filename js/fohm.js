614d9a6a37e2421fe6db8586d73c9163c6f47e2a
EBaxter1 <ebaxter1@hawk.iit.edu>
2016-12-04 17:26:15 -0600

    fixed index.html so that form is named right so that it can be referenced changed js file to make it work by actually outputting something

--- js/fohm.js
+++ js/fohm.js
@@ -1,18 +1,17 @@
 //TODO: Use a function closure and release global $
 $(document).ready(function() {
-//  $('#go').on('submit', function(event) {
-    //   event.preventDefault();
-   // var query = $('#name-label').val();
+   $('#name-form').on('submit', function(event) {
+   	var query = $('#namelabel').val();
     $.get(
       'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       function(data) {
-       // data.preventDefault();
         var q = data[0];
         console.log(q.content);
         console.log(q.title);
-        $('#primary').append(q.content);
-
+        $('#primary').append(q.content)
+         $('#username').append(query)
         });
-//});
+    event.preventDefault();
+});
 
 }); 
