096688fe53fa18d76db360066cee8c6fa980bb61
EBaxter1 <ebaxter1@hawk.iit.edu>
2016-12-05 19:22:10 -0600

    Fixed up files so everything was vaild and clean. Added lint files to test code

--- js/fohm.js
+++ js/fohm.js
@@ -1,13 +1,13 @@
 //TODO: Use a function closure and release global $
 $(document).ready(function() {
-   $('#name-form').on('submit', function(event) {
-   	var query = $('#namelabel').val();
+   $('#name_form').on('submit', function(event) {
+    var query = $('#namelabel').val();
     $.get(
       'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&cacheBuster=' + Math.random(),
       function(data) {
         var q = data[0];
-        console.log(q.content);
-        console.log(q.title);
+        console.log(q.content)
+        console.log(q.title)
         $('#primary').html(q.content)
          $('#username').html( 'Hello, ' +query + '! Your inspirational quote is below!')
         });
